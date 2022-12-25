d61aaf18a26e7c41797b3f1190c2b33f3b33b3d6
Mouahamed Mboup <mouhamed-el-moustapha.mboup@epitech.eu>
2022-12-25 14:23:21 +0100

    first commit

---
+++ api/src/test/java/com/metazo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metazo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
